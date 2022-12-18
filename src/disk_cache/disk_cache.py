2fbbdbf601f3aeda2cc0dc2454e4027c149ce26a
Robert Voelckner <robert.voelckner@gmail.com>
2022-12-18 09:55:09 +0100

    Added a few tests

--- src/disk_cache/disk_cache.py
+++ src/disk_cache/disk_cache.py
@@ -2,6 +2,7 @@ import inspect
 import pickle
 import shelve
 import shutil
+import time
 import uuid
 import warnings
 from datetime import datetime
@@ -51,7 +52,7 @@ class DiskCacheConfig(ABC):
 
 def disk_cache(
         cache_root_folder: Optional[Union[str, Path]] = None,
-        max_cache_root_size_mb: Optional[float] = None, max_cache_elements: Optional[int] = None,
+        max_cache_root_size_mb: Optional[float] = None, max_cache_instances: Optional[int] = None,
         iterable_loading_strategy: Literal["lazy-load-discard", "lazy-load-keep", "completely-load-to-memory"] =
                 "lazy-load-keep",
         cache_name_suffix: Optional[str] = None):
@@ -69,8 +70,8 @@ def disk_cache(
     @param cache_root_folder: The root folder that contains all cache instances. Cache instances are represented as
                             sub-folders within the cache root folder.
     @param max_cache_root_size_mb: Maximum size of the cache root folder in MB. If None, there is no size limit.
-    @param max_cache_elements: Maximum number of cache instances within the cache root folder. If None, there is no
-                               limit of cache instances.
+    @param max_cache_instances: Maximum number of cache instances within the cache root folder. If None, there is no
+                                limit of cache instances.
     @param iterable_loading_strategy: States how Iterable (List/Tuple/Generator) types should be handled when
                                       loading from cache.
                                       - Use "lazy-load-discard" to load each element from disk
@@ -80,11 +81,11 @@ def disk_cache(
     @param cache_name_suffix: If not None, this suffix will be part of the generated cache instance folder.
     @param log_fn: Function that's used for outputting log messages. If None, no log messages will be emitted.
     """
-    assert max_cache_elements is None or max_cache_elements > 0, "Value greater than 0 expected!"
+    assert max_cache_instances is None or max_cache_instances > 0, "Value greater than 0 expected!"
     assert max_cache_root_size_mb is None or max_cache_root_size_mb > 0, "Value greater than 0 expected!"
     assert iterable_loading_strategy in ("lazy-load-discard", "lazy-load-keep", "completely-load-to-memory"), \
         "Invalid iterable_loading_strategy parameter!"
-    if max_cache_root_size_mb is None and max_cache_elements is None and ENABLE_WARNINGS is True:
+    if max_cache_root_size_mb is None and max_cache_instances is None and ENABLE_WARNINGS is True:
         _msg = "Warning: It is not recommended to use an unlimited cache!"
         _LOGGER.warning(_msg)
         warnings.warn(_msg, stacklevel=3)
@@ -108,7 +109,7 @@ def disk_cache(
     def decorating_function(user_function):
         wrapper = _disk_cache_wrapper(
             user_function=user_function, cache_root_folder=cache_root_folder,
-            max_cache_root_size_mb=max_cache_root_size_mb, max_cache_elements=max_cache_elements,
+            max_cache_root_size_mb=max_cache_root_size_mb, max_cache_instances=max_cache_instances,
             iterable_loading_strategy=iterable_loading_strategy, cache_name_suffix=cache_name_suffix)
         return wrapper
 
@@ -133,7 +134,7 @@ def _get_config_from_params(user_function, args: Tuple, kwargs: Dict[str, Any]) 
 
 
 def _disk_cache_wrapper(user_function, cache_root_folder: Path, max_cache_root_size_mb: Optional[float],
-                        max_cache_elements: Optional[int], iterable_loading_strategy: str,
+                        max_cache_instances: Optional[int], iterable_loading_strategy: str,
                         cache_name_suffix: Optional[str]):
     def wrapper(*args, **kwargs) -> Any:
         _config = _get_config_from_params(user_function=user_function, args=args, kwargs=kwargs)
@@ -142,7 +143,7 @@ def _disk_cache_wrapper(user_function, cache_root_folder: Path, max_cache_root_s
                                              cache_root_folder=cache_root_folder)
         if _cache_instance_path is not None:
             _LOGGER.info(f"Compatible cache instance found: {_cache_instance_path.name}")
-            _cached_data = _load_cache_instance(cache_instance_path=_cache_instance_path,
+            _cached_data = _read_cache_instance(cache_instance_path=_cache_instance_path,
                                                 iterable_loading_strategy=iterable_loading_strategy)
             return _cached_data
 
@@ -180,10 +181,10 @@ def _disk_cache_wrapper(user_function, cache_root_folder: Path, max_cache_root_s
 
         # If we don't meet the user-imposed size limits, do some cleanup
         _cleanup(cache_root_folder=cache_root_folder, max_cache_root_size_mb=max_cache_root_size_mb,
-                 max_cache_elements=max_cache_elements, youngest_cache_instance_path=_cache_instance_path)
+                 max_cache_elements=max_cache_instances, youngest_cache_instance_path=_cache_instance_path)
 
-        # Deliver the cache now
-        _cached_data = _load_cache_instance(cache_instance_path=_cache_instance_path,
+        # Read the cache now and return the contents to the user
+        _cached_data = _read_cache_instance(cache_instance_path=_cache_instance_path,
                                             iterable_loading_strategy=iterable_loading_strategy)
         return _cached_data
 
@@ -227,18 +228,18 @@ def _lookup_cache(config_provided_to_user_function: DiskCacheConfig, cache_root_
     return None
 
 
-def _load_cache_instance(cache_instance_path: Path, iterable_loading_strategy: str) -> Any:
-    """Loads a cache instance. Distinguishes between simple cache "blobs" and iterables (shelves)."""
+def _read_cache_instance(cache_instance_path: Path, iterable_loading_strategy: str) -> Any:
+    """Reads a cache instance. Distinguishes between simple cache "blobs" and iterables (shelves)."""
     _LOGGER.debug(f"Start loading cache instance '{cache_instance_path.name}'")
     # Try writing to the last-usage-file, in order to keep track of most-recently used cache instance
     try:
         with open(file=cache_instance_path / _LAST_USAGE_FILENAME, mode="w") as file:
-            file.write("Nothing interesting in here.")
+            file.write(f"{time.time():.3f}")
     except IOError:
         # It's alright if we cannot write to this file!
         _LOGGER.debug(f"Couldn't write to last-usage file '{cache_instance_path.name}/{_LAST_USAGE_FILENAME}'.")
 
-    _is_iterable = any(filter(lambda path: path.name == _ITERABLE_CACHE_VALUE_FILENAME, cache_instance_path.iterdir()))
+    _is_iterable = any(p.name.startswith(_ITERABLE_CACHE_VALUE_FILENAME) for p in cache_instance_path.iterdir())
     if _is_iterable is True:
         _shelf_filename = cache_instance_path / _ITERABLE_CACHE_VALUE_FILENAME
         if iterable_loading_strategy in ("lazy-load-discard", "lazy-load-keep"):
@@ -261,12 +262,14 @@ class LazyList:
     def __init__(self, cache_file_path: Path, iterable_loading_strategy: str):
         self._cache_file_path = cache_file_path
         self._shelf = shelve.open(str(cache_file_path), flag="r")
+        self._is_open = True
         self._len = len(self._shelf)
         self._iterable_loading_strategy = iterable_loading_strategy
         # Prepare the storage for lazily loaded object, in case we want to keep them in memory
         if iterable_loading_strategy == "lazy-load-keep":
             self._storage_data_loaded: List[bool] = [False for _ in range(self._len)]
             self._storage: List[Any] = [None for _ in range(self._len)]
+            self._storage_n_loaded: int = 0
         # Make sure there are no gaps in the keys range. Note, that there are only numerical keys.
         valid_keys = range(len(self._shelf))
         assert all(int(k) in valid_keys for k in self._shelf.keys()), \
@@ -285,12 +288,15 @@ class LazyList:
         if self._iterable_loading_strategy == "lazy-load-keep":
             self._storage_data_loaded[idx] = True
             self._storage[idx] = datum
+            self._storage_n_loaded += 1
+            if self._storage_n_loaded == self._len:
+                self._close()
         return datum
 
     def __str__(self) -> str:
         if self._iterable_loading_strategy == "lazy-load-keep":
-            return f"[{len(self)} lazy objects, of which {sum(self._storage_data_loaded)} are held in RAM]"
-        return f"[{len(self)} lazy objects]"
+            return f"[{self._len} lazy objects, of which {self._storage_n_loaded} are held in RAM]"
+        return f"[{self._len} lazy objects]"
 
     def __hash__(self):
         return hash(self._len) + hash(self._cache_file_path)
@@ -303,12 +309,18 @@ class LazyList:
         return True
 
     def __iter__(self):
-        for idx in range(len(self)):
+        for idx in range(self._len):
             yield self[idx]
 
-    def __del__(self):
+    def _close(self):
+        if self._is_open is False:
+            return
         _LOGGER.debug(f"Successfully closed the shelf for '{self._cache_file_path.parent.name}'")
         self._shelf.close()
+        self._is_open = False
+
+    def __del__(self):
+        self._close()
 
 
 def _create_new_cache_path(cache_root_folder: Path, cache_name_suffix: Optional[str]) -> Path:
@@ -328,6 +340,21 @@ def _get_cache_root_folder_stats(cache_root_folder: Path) -> Dict[str, Any]:
     return {"n_cache_instances": n_instances, "cache_root_size_mb": overall_size_mb}
 
 
+def _get_cache_instance_last_usage_time(cache_instance_path: Path) -> float:
+    """Returns the last usage of a cache instance, as seconds since epoch."""
+    _last_usage_file = cache_instance_path / _LAST_USAGE_FILENAME
+    if _last_usage_file.is_file():
+        try:
+            with open(_last_usage_file, mode="r", encoding="utf-8") as _file:
+                return float(_file.readline())
+        except (IOError, ValueError):
+            if ENABLE_WARNINGS is True:
+                _LOGGER.warning(f"Contents of last-usage file '{cache_instance_path}/{_LAST_USAGE_FILENAME}' could "
+                                f"not be parsed!")
+            return _last_usage_file.stat().st_mtime
+    return cache_instance_path.stat().st_mtime  # Acts as fallback, if (for whatever reason) no last-usage file exists
+
+
 def _cleanup(cache_root_folder: Path, max_cache_root_size_mb: Optional[float], max_cache_elements: Optional[int],
              youngest_cache_instance_path: Path) -> None:
     def _get_cache_root_status_quo() -> Tuple[int, float, bool]:
@@ -337,16 +364,12 @@ def _cleanup(cache_root_folder: Path, max_cache_root_size_mb: Optional[float], m
             max_cache_elements is not None and _stats_dict["n_cache_instances"] > max_cache_elements
         return _stats_dict["n_cache_instances"], _stats_dict["cache_root_size_mb"], _cleanup_necessary
 
-    def _get_instance_usage_time(_folder: Path) -> float:
-        _element_of_interest = (_folder/_LAST_USAGE_FILENAME) if (_folder/_LAST_USAGE_FILENAME).is_file() else _folder
-        return _element_of_interest.stat().st_mtime
-
     _n_instances, _overall_size_mb, _cleanup_necessary = _get_cache_root_status_quo()
     if _cleanup_necessary is True:
         _LOGGER.info(f"Cache root folder exceeds its limits, start doing some cleanup..   "
                      f"(overall_cache_size_mb={_overall_size_mb:,.1f}, n_cache_instances={_n_instances:,})")
         _n_deleted = 0
-        _instance_usage_time__paths = {_get_instance_usage_time(path): path for path in cache_root_folder.iterdir() if
+        _instance_usage_time__paths = {_get_cache_instance_last_usage_time(path): path for path in cache_root_folder.iterdir() if
                                        path.is_dir() and path != youngest_cache_instance_path}
         _sorted_deletable_paths = [p for _, p in sorted(_instance_usage_time__paths.items(), key=lambda item: item[0])]
         while _n_instances >= 2 and _cleanup_necessary is True:
@@ -363,4 +386,4 @@ def _cleanup(cache_root_folder: Path, max_cache_root_size_mb: Optional[float], m
             _LOGGER.warning(_msg)
         else:
             _LOGGER.info(f"Successfully removed {_n_deleted} cache instances. There are {_n_instances} remaining.")
-        assert _n_instances >= 1  # "Fear-assert" to make sure the above code works correctly
+        assert _n_instances >= 1  # Makes sure we end up with at least the lastly generated cache instance..
--- tests/test_disk_cache.py
+++ tests/test_disk_cache.py
@@ -3,7 +3,7 @@ import warnings
 from dataclasses import dataclass
 from pathlib import Path
 from tempfile import TemporaryDirectory
-from typing import Dict, Any
+from typing import Dict, Any, Callable
 
 import pytest
 
@@ -24,6 +24,14 @@ class _DummyConfig(DiskCacheConfig):
     @staticmethod
     def _from_dict(dict_: Dict[str, Any]) -> "DiskCacheConfig":
         return _DummyConfig(a=dict_["a"], b=dict_["b"])
+
+
+def _assert_duration(duration: float, fn: Callable, *args, **kwargs) -> Any:
+    _started_at = time.time()
+    _value = fn(*args, **kwargs)
+    _duration = time.time() - _started_at
+    assert (duration-0.1) <= _duration <= (duration+0.1), f"Duration of {_duration:.3f}s was not in the expected range!"
+    return _value
 
 
 def test_different_function_parameters(_temp_dir):
@@ -50,19 +58,11 @@ def test_different_function_parameters(_temp_dir):
     pytest.raises(AssertionError, _long_running_function__no_param)
     pytest.raises(AssertionError, _long_running_function__param_but_not_the_right_one, number=123)
     assert _long_running_function__single_param(_DummyConfig(1, "2")) == 42
+    assert _long_running_function__multiple_params(_DummyConfig(1, "hello"), 22) == 24
 
     # Make sure a warning is emitted ("decorated function expects too many arguments")
-    assert _long_running_function__multiple_params(_DummyConfig(1, "hello"), 22) == 24
     warnings.filterwarnings("error")  # Makes warnings being raised as errors
     pytest.raises(UserWarning, _long_running_function__multiple_params, _DummyConfig(1, "hello"), 23)
-
-
-def test_using_timings(_temp_dir):
-    pass
-
-
-def test_iterables(_temp_dir):
-    pass
 
 
 def test_helper_functions__cache_info(_temp_dir):
@@ -84,8 +84,91 @@ def test_helper_functions__cache_info(_temp_dir):
 
 
 def test_helper_functions__cache_clear(_temp_dir):
-    pass
+    @disk_cache(cache_root_folder=_temp_dir)
+    def _dummy_fn(config: _DummyConfig):
+        time.sleep(0.3)
+        return config.a
+
+    _config_param = _DummyConfig(1, "1")
+
+    _value = _assert_duration(0.3, _dummy_fn, _config_param)
+    assert _value == _config_param.a
+
+    _value = _assert_duration(0.0, _dummy_fn, _config_param)
+    assert _value == _config_param.a
+
+    _dummy_fn.cache_root_clear()
+
+    _value = _assert_duration(0.3, _dummy_fn, _config_param)
+    assert _value == _config_param.a
 
 
 def test_cleanup(_temp_dir):
-    pass
+    @disk_cache(cache_root_folder=_temp_dir, max_cache_instances=2)
+    def _dummy_fn(config: _DummyConfig):
+        time.sleep(0.3)
+        return config.a
+
+    # Build up cache of two cache instances
+    _value = _assert_duration(0.3, _dummy_fn, _DummyConfig(1, "1"))
+    assert _value == 1
+    _value = _assert_duration(0.3, _dummy_fn, _DummyConfig(2, "2"))
+    assert _value == 2
+
+    # Read cache instance #1 and thus mark it as "recently used"
+    _value = _assert_duration(0.0, _dummy_fn, _DummyConfig(1, "1"))
+    assert _value == 1
+
+    # Generate new cache instance #3 and override instance #2, since it was not recently used
+    _value = _assert_duration(0.3, _dummy_fn, _DummyConfig(3, "3"))
+    assert _value == 3
+
+    # Make sure that instance #2 was really deleted
+    _value = _assert_duration(0.3, _dummy_fn, _DummyConfig(2, "2"))
+    assert _value == 2
+
+    # Verify cache instances #2 and #3 are present
+    _value = _assert_duration(0.0, _dummy_fn, _DummyConfig(2, "2"))
+    assert _value == 2
+    _value = _assert_duration(0.0, _dummy_fn, _DummyConfig(3, "3"))
+    assert _value == 3
+
+    # Generate new cache instance #4 and override instance #2, since it was not recently used
+    _value = _assert_duration(0.3, _dummy_fn, _DummyConfig(4, "4"))
+    assert _value == 4
+
+
+def test_iterables__list(_temp_dir):
+    @disk_cache(cache_root_folder=_temp_dir, iterable_loading_strategy="completely-load-to-memory")
+    def _dummy_fn(config: _DummyConfig):
+        time.sleep(0.3)
+        return [config.b for _ in range(config.a)]
+
+    _values = _assert_duration(0.3, _dummy_fn, _DummyConfig(1000, "text"))
+    assert isinstance(_values, list) and len(_values) == 1000 and all(v == _values[0] for v in _values)
+
+    _values = _assert_duration(0.0, _dummy_fn, _DummyConfig(1000, "text"))
+    assert isinstance(_values, list) and len(_values) == 1000 and all(v == _values[0] for v in _values)
+
+
+def test_iterables__lazy_list_and_generator(_temp_dir):
+    @disk_cache(cache_root_folder=_temp_dir, iterable_loading_strategy="lazy-load-keep")
+    def _dummy_fn(config: _DummyConfig):
+        time.sleep(0.3)
+        for _ in range(config.a):
+            yield config.b
+
+    _values = _assert_duration(0.3, _dummy_fn, _DummyConfig(500, "text"))
+    assert isinstance(_values, LazyList) and len(_values) == 500 and all(v == _values[0] for v in _values)
+
+    _values = _assert_duration(0.0, _dummy_fn, _DummyConfig(500, "text"))
+    assert isinstance(_values, LazyList) and len(_values) == 500 and all(v == _values[0] for v in _values)
+
+    # Test LazyList's iterator
+    for _item in _values:
+        assert _item == "text"
+
+    # Check what happens when we read a cached Iterable, and purge the cache right away
+    _values = _assert_duration(0.0, _dummy_fn, _DummyConfig(500, "text"))
+    _dummy_fn.cache_root_clear()
+    pytest.raises(FileNotFoundError, lambda: _values[0])
